c9c99631e4613b396b934d1645fa88b60f3efc4a
Yelkira <penda.fu@gmail.com>
2022-12-07 20:56:20 +0300

    -

--- freecodcamps/Basic JS/92.js
+++ freecodcamps/Basic JS/92.js
@@ -1,1 +1,13 @@
-xxx
+const myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
