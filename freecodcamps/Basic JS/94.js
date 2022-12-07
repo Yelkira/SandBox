d183d00d682b04b84b610d5ca5dd1b3ef7f68f35
Yelkira <penda.fu@gmail.com>
2022-12-07 21:03:30 +0300

    -

--- freecodcamps/Basic JS/94.js
+++ freecodcamps/Basic JS/94.js
@@ -24,7 +24,7 @@ function updateRecords(records, id, prop, value) {
     if (value === '') {
         delete records[id][prop];
     } else if (prop === 'tracks') {
-        records[id][prop] = records[id][prop] || []; // this is called shortcircuit evaluation, see below for explanation
+        records[id][prop] = records[id][prop] || [];
         records[id][prop].push(value);
     } else {
         records[id][prop] = value;
